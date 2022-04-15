0cc4fb7b21be5b0cc4449b8d8919924ce97bfb60
amandagrono <59928641+amandagrono@users.noreply.github.com>
2022-04-14 21:55:26 -0400

    created project

---
+++ src/main/java/com/example/moviematchserver/MovieMatchServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.moviematchserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMatchServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieMatchServerApplication.class, args);
+    }
+
+}
